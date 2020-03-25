1afc4ed6f040206054dd8a35ffc13ef6f26a1953
DeCyberPrince <dyx.013@gmail.com>
2020-03-25 18:30:50 +0500

    reconfigured

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-const path = require("path");
 module.exports = {
   productionSourceMap: false,
-  assetsDir: "assets",
-  outputDir: path.resolve(__dirname, "../server/public")
+  assetsDir: "assets"
 };
